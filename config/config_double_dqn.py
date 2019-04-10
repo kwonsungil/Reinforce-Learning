2f3068d100a351366d023260469ec504d92482d6
kwonsungil <thuntherll@naver.com>
2019-04-11 00:35:34 +0900

    Add files via upload

---
+++ config/config_double_dqn.py
@@ -0,0 +1,24 @@
+import os
+
+GAMMA = 0.9
+START_EPSILON = 0.2
+END_EPSILON = 0.01
+REPLAY_SIZE = 50000
+BATCH_SIZE = 16
+HIDDEN_SIZE = 20
+MAX_EPISODES = 5000
+MAX_STEPS = 300
+TARGET_UPDATE_INTERVAL = 20
+
+out_dir = os.path.join('./', "runs", 'double_dqn')
+summary_dir = os.path.join(out_dir, "summaries")
+checkpoint_dir = os.path.join(out_dir, "checkpoints")
+os.makedirs(out_dir, exist_ok=True)
+os.makedirs(summary_dir, exist_ok=True)
+os.makedirs(checkpoint_dir, exist_ok=True)
+
+
+initial_learning_rate = 0.0001
+decay_steps = 1000
+decay_rate = 0.95
+staircase = True
